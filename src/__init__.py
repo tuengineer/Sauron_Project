84db78f1efd9f083c98a400553f049ce50d0083b
tuengineer <moguco@gmail.com>
2026-02-26 19:09:35 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,4 @@
-# Vacío para permitir imports
+from .core import MarketSimulator
+from .orderbook import OrderBook
+from .latency_model import LatencyModel
+from .flow_generator import FlowGenerator# Vacío para permitir imports
